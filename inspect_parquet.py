081749b7ca6703378fab973933621b410a9e6f74
tyhowee <tyhowe@outlook.com>
2024-11-17 18:20:28 -0700

    categorical handling updated, but slows the function a bit. working on a fix

--- inspect_parquet.py
+++ inspect_parquet.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 # Replace 'your_file.parquet' with the path to your Parquet file
-file_path = '/Users/thowe/MinersAI Dropbox/Tyler Howe/ICB_data/cube_testing/08_zero_layers/(200, 200)/checker1_df.parquet'
+file_path = r"C:\Users\TyHow\MinersAI Dropbox\Tyler Howe\ICB_data\full_mines_cubes_2\set_1\(200, 200)\complete_df.parquet"
+
+print(f'\nInspecting file: {file_path}\n')
 
 # Load the Parquet file
 df = pd.read_parquet(file_path)
@@ -13,5 +15,3 @@ print("\nSample Data:")
 print(df.head())
 print("\nColumns:")
 
-print(f'GEOL DTYPE: {df['ICB_GEOL_dumbed_dumb_geol'].dtype}')
-print(df.columns)
